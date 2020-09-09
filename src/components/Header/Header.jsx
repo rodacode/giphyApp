1f80a6a0a64f0fad0dc55ddc93089857999e0819
Nicolas Hardmeier <nicolas.hardmeier@travelport.com>
2020-09-10 01:34:34 +0200

    feat(added header component)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [inputValue, setInputValue] = useState();
+
   return (
-<>
-</>
+    <div className="header__container" data-testid="header__container">
+      <input value={inputValue} />
+      <button onClick={()=>{}}>Search</button>
+    </div>
   );
 };
 
