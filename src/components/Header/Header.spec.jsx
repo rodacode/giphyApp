29094b9d4ad18c8195cd34535e0f36e128ff3a4d
Nicolas Hardmeier <nicolas.hardmeier@travelport.com>
2020-09-10 01:29:37 +0200

    test(added test to render header component)

---
+++ src/components/Header/Header.spec.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+import { render, screen } from '@testing-library/react';
+
+describe('Header [COMPONENT]', () => {
+
+    beforeEach(async () => {
+        render(<Header />);
+    })
+    test('renders header main container', async () => {
+        expect(screen.getByTestId('header__container')).toBeInTheDocument()
+      });
+});
