99863caf9aa78dd8e8d6b80d17dafed3c02b457e
Nicolas Hardmeier <nicolas.hardmeier@travelport.com>
2020-09-10 10:44:52 +0200

    feat(added a home page)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import GifsList from "../../components/GifsList/GifsList";
 
 const Home = () => {
-
   return (
-    <>
-      </>
+    <div className="home__container" data-testid="home__container">
+      <Header />
+      <GifsList />
+    </div>
   );
 };
 
