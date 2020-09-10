f274b39567e4a7a9d559b3b0cbe9777f59aadedc
Nicolas Hardmeier <nicolas.hardmeier@travelport.com>
2020-09-10 15:07:54 +0200

    feat(added search input and button)

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 
 const Search = () => {
+  const [inputValue, setInputValue] = useState();
 
+  return (
+    <div className="search__container" data-testid="search__container">
+      <input value={inputValue} data-testid="search__input" />
+      <button onClick={()=>{}} data-testid="search__button">Search</button>
+    </div>
+  );
 };
 
 export default Search;
--- src/components/search/Search.spec.js
+++ src/components/search/Search.spec.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import Search from './Search';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import Search from "./Search";
+import { render, screen } from "@testing-library/react";
 
-describe('Search [COMPONENT]', () => {
-
-    beforeEach(async () => {
-        render(<Search />);
-    })
-    test('renders search container', async () => {
-        expect(screen.getByTestId('search__container')).toBeInTheDocument();
-      });
+describe("Search [COMPONENT]", () => {
+  beforeEach(async () => {
+    render(<Search />);
+  });
+  test("it should render the search container", async () => {
+    expect(screen.getByTestId("search__container")).toBeInTheDocument();
+  });
+  test("it should render the search input", async () => {
+    expect(screen.getByTestId("search__input")).toBeInTheDocument();
+  });
+  test("it should render the search button", async () => {
+    expect(screen.getByTestId("search__button")).toBeInTheDocument();
+  });
 });
