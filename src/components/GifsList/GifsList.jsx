de15c8e3f294d54f0170bf035403ccb412a2629c
Nicolas Hardmeier <nicolas.hardmeier@travelport.com>
2020-09-10 12:24:49 +0200

    feat(added Gif react component)

--- src/components/GifsList/GifsList.jsx
+++ src/components/GifsList/GifsList.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { Gif } from "@giphy/react-components";
 
-const GifsList = ({gifs}) => {
+const GifsList = ({ gifs }) => {
+  const onGifClick = (gif, e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="gifsList__container" data-testid="gifsList__container">
-      {gifs && gifs.map((gif) => (
-        <div key={gif.id} data-testid="gif__item">
-          <p>{gif.title}</p>
-        </div>
-      ))}
+      {gifs &&
+        gifs.map((gif) => (
+          <div data-testid="gif__item">
+            <Gif
+              key={gif.id}
+              gif={gif}
+              width={"100%"}
+              onGifClick={onGifClick}
+            />
+          </div>
+        ))}
     </div>
   );
 };
