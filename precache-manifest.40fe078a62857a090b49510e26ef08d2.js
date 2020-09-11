ca589ca8ac600d35acdf877cdfff7c13aedfc198
Nicolas Hardmeier <nicolas.hardmeier@travelport.com>
2020-09-11 23:12:41 +0200

    Updates

---
+++ precache-manifest.40fe078a62857a090b49510e26ef08d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3978682b9c4391354ab1b1629a77385f",
+    "url": "/giphyApp/index.html"
+  },
+  {
+    "revision": "9ff9bed21c406059c1f9",
+    "url": "/giphyApp/static/css/main.f711d931.chunk.css"
+  },
+  {
+    "revision": "0a12d5c054ae42cfb28e",
+    "url": "/giphyApp/static/js/2.3f31921a.chunk.js"
+  },
+  {
+    "revision": "9ab64b88dc192d97944f0aceba883cba",
+    "url": "/giphyApp/static/js/2.3f31921a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ff9bed21c406059c1f9",
+    "url": "/giphyApp/static/js/main.e486bd86.chunk.js"
+  },
+  {
+    "revision": "6c9a269eda3b62bc934a",
+    "url": "/giphyApp/static/js/runtime-main.4f74bd7e.js"
+  }
+]);
